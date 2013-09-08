3e39b25c1783794d8686162ee1db6d655cb06a44
sreejitr <sreejitr@andrew.cmu.edu>
2013-09-08 04:49:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheVirtualLibrarySree::Application.config.secret_key_base = 'f9fa8a3781b156e63ed81dc14ea5c8f0ab3e64e95cd9143b35b9e9d260531fbc31f2ff92f1a9409d0533c1f500f21ece0fc9482d4a80ff938966c623b03e82a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheVirtualLibrarySree::Application.config.session_store :cookie_store, key: '_the_virtual_library_sree_session'
